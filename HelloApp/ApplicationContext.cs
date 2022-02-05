4145a734a1c01e6a1bd4431de118cb02923a895e
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-02-05 17:13:48 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HelloApp
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database = helloappdb; Trusted_Connection = True;");
+        }
+    }
+}
